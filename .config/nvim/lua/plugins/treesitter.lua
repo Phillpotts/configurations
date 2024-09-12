2de5eaaeb5579ecd6e83cc3858d348f0abb8ac42
william phillpotts <william.phillpotts@sky.uk>
2024-09-12 15:17:31 +0100

    treesitter plugin rename

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 	local configs = require("nvim-treesitter.configs")
 
 	configs.setup({
-	    ensure_installed = { "lua", "python", "json", "markdown" },
+	    ensure_installed = { "lua", "python", "json", "markdown", "terraform" },
 	    sync_install = false,
 	    highlight = { enable = true },
 	    indent = { enable = true },
